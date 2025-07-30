c3ba561b2e6bdffe0b30526799a3424675154dd4
Sriram Suresh <sriram.suresh449@gmail.com>
2025-07-30 08:16:07 -0700

    added vimtex config

--- init.lua
+++ init.lua
@@ -1026,6 +1026,15 @@ require('lazy').setup({
       }
     end,
   },
+  {
+    "lervag/vimtex",
+    lazy = false,     -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = "zathura"
+    end
+  },
   -- install without yarn or npm
   -- Install markdown preview, use npx if available.
   {
